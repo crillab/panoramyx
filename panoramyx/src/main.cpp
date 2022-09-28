3aa2a43bf36a50d68ac54acd84de80400d82add7
tfalque.ext <thibault_falque@ens.univ-artois.fr>
2022-09-28 14:46:26 +0200

    :fire: remove submodule

---
+++ panoramyx/src/main.cpp
@@ -0,0 +1,8 @@
+/**
+* @date 28/09/22
+* @file ${HEADER_FILENAME}
+* @brief 
+* @author Thibault Falque
+* @author Romain Wallon 
+* @license This project is released under the GNU LGPL3 License.
+*/
