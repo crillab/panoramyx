b2e0c490b2b6930a6a08a1f7ecd6d1bd58d166a3
tfalque.ext <thibault_falque@ens.univ-artois.fr>
2023-02-07 17:57:19 +0100

    Updates lib autis

---
+++ panoramyx/include/solver/PortfolioSolverBuilder.hpp
@@ -0,0 +1,33 @@
+/**
+* @date 07/02/23
+* @file PortfolioSolverBuilder.hpp
+* @brief 
+* @author Thibault Falque
+* @author Romain Wallon 
+* @license This project is released under the GNU LGPL3 License.
+*/
+
+
+#ifndef PANORAMYX_PORTFOLIOSOLVERBUILDER_HPP
+#define PANORAMYX_PORTFOLIOSOLVERBUILDER_HPP
+
+namespace Panoramyx {
+
+/**
+ * @class PortfolioSolverBuilder
+ *
+ * @brief 
+ * @file PortfolioSolverBuilder.hpp
+ * @author Thibault Falque
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+
+    class PortfolioSolverBuilder {
+
+    };
+
+} // Panoramyx
+
+#endif //PANORAMYX_PORTFOLIOSOLVERBUILDER_HPP
---
+++ panoramyx/src/solver/PortfolioSolverBuilder.cpp
@@ -0,0 +1,21 @@
+/**
+* @date 07/02/23
+* @file PortfolioSolverBuilder.cpp
+* @brief 
+* @author Thibault Falque
+* @author Romain Wallon 
+* @license This project is released under the GNU LGPL3 License.
+*/
+
+
+#include "PortfolioSolverBuilder.hpp"
+
+namespace Panoramyx {
+
+/**
+@class PortfolioSolverBuilder
+@brief Definition of the class PortfolioSolverBuilder. 
+@file PortfolioSolverBuilder.cpp
+*/
+
+} // Panoramyx
